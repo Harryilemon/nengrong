36d9c38326ecc6cd54d4ecd29912e531cd9e58e6
cebqby <122104615@qq.com>
2016-02-20 13:55:09 +0800

    ddd

--- Home/View/src/scripts/is-operationLog.js
+++ Home/View/src/scripts/is-operationLog.js
@@ -1,5 +1,8 @@
 $(function() {
 
+	$(".l-nav").find(".operationLog").addClass("active")
+			.children("a").attr("href", "javascript:;");
+
 	/* 分页 */
 	$(".pager span").click(function() {
 		var $this = $(this);
